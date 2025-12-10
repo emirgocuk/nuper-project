1d81367c0a3f53bcd0e185d31e776d82ecf7f9c6
Emir Göçük <51445495+emirgocuk@users.noreply.github.com>
2025-12-11 02:11:59 +0300

    Add type declarations for Editor.js modules
    Introduces module declarations for various @editorjs plugins to support TypeScript compatibility and prevent type errors when importing these modules.

---
+++ nuper-next-postgresql/src/types/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '@editorjs/embed';
+declare module '@editorjs/link';
+declare module '@editorjs/marker';
+declare module '@editorjs/header';
+declare module '@editorjs/list';
+declare module '@editorjs/quote';
+declare module '@editorjs/image';
+declare module '@editorjs/simple-image';
